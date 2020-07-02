93f3895bf34f27659e8a6d37869f6573c85eab12
Matthew Hemingway <dpplrdr435@gmail.com>
2020-07-01 22:27:46 -0400

    Added relations between the models

--- app/Task.php
+++ app/Task.php
@@ -8,8 +8,23 @@ use Illuminate\Database\Eloquent\Model;
  * Class Task
  * @package App
  *
- * @property
+ * @property integer id
+ * @property string title
+ * @property string description
+ * @property boolean status
+ * @property string completed_at
+ * @property string updated_at
+ * @property string deleted_at
  */
 class Task extends Model
 {
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function priorities()
+    {
+        return $this->belongsToMany(TaskPriority::class)->withTimestamps();
+    }
 }
--- app/TaskPriority.php
+++ app/TaskPriority.php
@@ -4,7 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class TaskPriority
+ * @package App
+ *
+ * @property integer id
+ * @property string type
+ */
 class TaskPriority extends Model
 {
-    //
+    public function tasks()
+    {
+        return $this->belongsToMany(Task::class);
+    }
 }
