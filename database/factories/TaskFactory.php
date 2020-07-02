a57c44efc471a973f349462e9598a973580f10b6
Matthew Hemingway <dpplrdr435@gmail.com>
2020-07-02 00:00:23 -0400

    Added TaskFactory and seeders to add initial priorities and users.

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,11 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Task;
-use Faker\Generator as Faker;
-
-$factory->define(Task::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
+//use App\User as User;
+//use App\Task;
+//use Faker\Generator as Faker;
+//
+//$factory->define(Task::class, function (Faker $faker) {
+//    return [
+//        'user_id' => factory(User::class),
+//        'title'=> $faker->sentence,
+//        'description' => $faker->paragraph
+//    ];
+//});
--- database/seeds/TaskPrioritySeeder.php
+++ database/seeds/TaskPrioritySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB as DB;
 
 class TaskPrioritySeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class TaskPrioritySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('task_priorities')->insert([
+            ['type' => 'urgent'],
+            ['type' => 'important'],
+            ['type' => 'ignore'],
+            ['type' => 'optional'],
+        ]);
     }
 }
